e700c2c93d9e6a3218792f49f511a022f91c221e
Yassine Abidi <yabidi@smb.sqli.com>
2024-03-22 10:22:55 +0000

    (WIP) add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+    return (
+      <header>
+        <div className="top-bar w-full bg-black h-11 text-white text-right text-sm font-semibold flex justify-end pr-12"><div className=" h-full  w-32 text-center pt-3 cursor-pointer hover:bg-gray-800">MY ACCOUNT</div></div>
+        <div className="head"></div>
+        <div className="navbar"></div>
+      </header>
+    )
+  }
+  
+  export default Navbar
+  
