5b6de601dd72f217d425c1a344db23162c2c8a2a
Yassine <control.yassine@gmail.com>
2024-03-23 15:44:59 +0000

    feat: implement navbar & search bar

---
+++ src/components/logo.tsx
@@ -0,0 +1,12 @@
+function Logo(){
+
+    return (
+        <div className="h-24 flex items-center justify-center">
+          <img className="h-9" src="../../public/logo.png">
+            </img>
+        </div>
+    )
+    
+    }
+    
+    export default Logo
