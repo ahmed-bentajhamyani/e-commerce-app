60d5b19c78536ba0c38b4b79aa2f507a5fd4eacc
Rabbitou <48840164+Rabbitou@users.noreply.github.com>
2024-03-23 17:47:19 +0000

    feat:add SwiperBox component with swiper library

---
+++ src/components/SwiperBox.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode } from "react";
+import { Swiper } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import { Autoplay, FreeMode, Pagination } from "swiper/modules";
+
+export default function SwiperBox({ children }: { children: ReactNode }) {
+  return (
+    <Swiper
+      modules={[FreeMode, Pagination, Autoplay]}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+        waitForTransition: true,
+      }}
+      speed={1000}
+      slidesPerView="auto"
+      freeMode={true}
+      className="mySwiper"
+      spaceBetween={20}
+    >
+      {children}
+    </Swiper>
+  );
+}
